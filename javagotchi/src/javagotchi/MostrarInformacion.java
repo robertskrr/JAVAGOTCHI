5e74b93362637655aebda643bcc03545ebad5f5f
Robert Esquerre <esquerre.valiente.robert.ernesto@iescamas.es>
2025-05-13 20:17:25 +0200

    Añadida interfaz MostrarInformacion

---
+++ javagotchi/src/javagotchi/MostrarInformacion.java
@@ -0,0 +1,13 @@
+package javagotchi;
+
+/**
+ * @author Robert
+ * @version 1.0
+ * @since 13/05
+ */
+public interface MostrarInformacion {
+	/**
+	 * Muestra la información del objeto
+	 */
+	void mostrarInfo();
+}
