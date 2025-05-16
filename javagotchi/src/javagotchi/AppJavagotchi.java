a67e65bba31868451f00cd7eee86eab394ac7396
Robert Esquerre <esquerre.valiente.robert.ernesto@iescamas.es>
2025-05-16 15:25:39 +0200

    Creada clase APP (Vacia de momento)

---
+++ javagotchi/src/javagotchi/AppJavagotchi.java
@@ -0,0 +1,24 @@
+package javagotchi;
+
+import java.util.*;
+
+/**
+ * @author Robert
+ * @version 1.0
+ * @since 16/05
+ */
+public class AppJavagotchi {
+
+	private static UsuarioDAO bdUser = new UsuarioDAO();
+
+	private static MascotaDAO bdMascota = new MascotaDAO();
+
+	private static ComidaDAO bdComida = new ComidaDAO();
+
+	private static JuegoDAO bdJuego = new JuegoDAO();
+
+	public static void main(String[] args) {
+
+	}
+
+}
